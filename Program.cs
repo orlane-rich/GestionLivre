01548aafe1d782f5f7f416214a375d76402c93c8
Orlane Segolene Ketcha <orlanesegolene.ketcha@monboreal.ca>
2024-10-21 13:39:51 -0400

    Ajout du fichier Revues.cs et de Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using AppConsoleLivres;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Revue revue1 = new Revue("Pere riche Pere pauvre", "Robert T. Kiyosaki", 332, 12, 1997);
+        Revue revue2 = new Revue("Aya de Yopougon", "Marguerite Abouet", 104, 9, 2023);
+        Revue revue3 = new Revue("Toujours Plus", "Lena Mahfouf", 152, 1, 2020);
     }
 }
---
+++ Revue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Revue : Livre
+    {
+        
+    // Definition des differentes Propriétés 
+    public int Numero { get; set; }
+        public int Annee { get; set; }
+
+
+        // Ajout des Constructeurs
+        public Revue(string titre, string auteur, int nombrePages, int numero, int annee) : base(titre, auteur, nombrePages)
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+
+
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Le Titre est: {Titre}, son Auteur est: {Auteur}, son Nombre de pages est: {NombrePages}, et enfin le Numéro: {Numero}, Année: {Annee}");
+        }
+    }
+}
