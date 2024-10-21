a6cbcdff73cc89a0f77ec3806c035285d47c0ec4
Orlane Segolene Ketcha <orlanesegolene.ketcha@monboreal.ca>
2024-10-21 14:10:45 -0400

    Ajout du fichier Roman.cs et Program.cs

--- Program.cs
+++ Program.cs
@@ -7,5 +7,9 @@ internal class Program
         Revue revue1 = new Revue("Pere riche Pere pauvre", "Robert T. Kiyosaki", 332, 12, 1997);
         Revue revue2 = new Revue("Aya de Yopougon", "Marguerite Abouet", 104, 9, 2023);
         Revue revue3 = new Revue("Toujours Plus", "Lena Mahfouf", 152, 1, 2020);
+
+        Roman roman1 = new Roman("La Nausee", "Jean Paul Sartre", 249, "Autobiographique");
+        Roman roman2 = new Roman("Les Rois Maudits", "Maurice Druon", 2149, "Historique");
+        Roman roman3 = new Roman("Le Horla", "Guy de Maupassant", 143, "Fantastique");
     }
 }
---
+++ Roman.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Roman : Livre
+    {
+       
+            // Definition des Propriétés de roman
+            public string Genre { get; set; }
+
+            // Definition du Constructeur de roman
+            public Roman(string titre, string auteur, int nombrePages, string genre) : base(titre, auteur, nombrePages)
+            {
+                Genre = genre;
+            }
+
+
+            public override void AfficherDetails()
+            {
+                Console.WriteLine($"Titre: {Titre}, Auteur: {Auteur}, Nombre de pages: {NombrePages}, Genre: {Genre}");
+            }
+        }
+}
